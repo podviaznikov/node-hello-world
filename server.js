c9600bbb8ae8fea6e0f3df43234264b2d804394c
Anton Podviaznikov <podviaznikov@gmail.com>
2017-02-22 17:38:31 -0800

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-    res.send('Hello Anton.....4');
+    res.send('Hello Anton.....5');
 });
 
 app.listen(80);
